1c82faa06504e31c078018c8a90d6e4e1ef6dfc7
Luis <vsjoseluisvs@gmail.com>
2021-09-01 10:02:51 -0500

    fix: questions controller file structure changed

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,24 +1,44 @@
-module Api
-    module V1
-        class CommentsController < ApplicationController
-            # protect_from_forgery with: :null_session
-            # before_action :authenticate_user!
+class Api::V1::CommentsController < ApplicationController
+    before_action :set_commentable
 
-            def create
-                comment = @commentable.comments.new comment_params
-                # @comment.user = current_user
+    private
 
-                comment.save
-
-                # Write if condition TO DO
-                #redirect_to @commentable, notice: "Your comment was successfully posted."
-            end
-
-            private
-
-            def comment_params
-                params.require(:comment).permit(:text)
-            end
-        end
+    def set_commentable
+        @commentable ||= Question.find(params[:question_id])
+        puts "@commentable #{@commentable}"
     end
 end
+
+# module Api
+#     module V1
+#         class CommentsController < ApplicationController
+#             # protect_from_forgery with: :null_session
+#             # before_action :authenticate_user!
+            
+#             def index
+#                 comments = @commentable.comments.all
+            
+#                 render json: CommentSerializer.new(comments).serialized_json
+#             end
+
+#             def create
+#                 comment = @commentable.comments.new comment_params
+#                 # @comment.user = current_user
+
+#                 if comment.save
+#                     render json: CommentSerializer.new(comment).serialized_json
+#                 else
+#                     render json: { error: exam.errors.messages }, status: 422
+#                 end
+
+#                 #redirect_to @commentable, notice: "Your comment was successfully posted."
+#             end
+
+#             private
+
+#             def comment_params
+#                 params.require(:comment).permit(:text)
+#             end
+#         end
+#     end
+# end
--- app/controllers/api/v1/questions/comments_controller.rb
+++ app/controllers/api/v1/questions/comments_controller.rb
@@ -1,16 +1,47 @@
 class Api::V1::Questions::CommentsController < Api::V1::CommentsController
-    before_action :set_commentable
+            # protect_from_forgery with: :null_session
+            # before_action :authenticate_user!
+            
+            def index
+                comments = @commentable.comments.all
+            
+                render json: CommentSerializer.new(comments).serialized_json
+            end
 
-    # def commentable
-    #     @commentable ||= Question.find(params[:question_id])
-    # end
+            def create
+                comment = @commentable.comments.new comment_params
+                # @comment.user = current_user
 
-    private
+                if comment.save
+                    render json: CommentSerializer.new(comment).serialized_json
+                else
+                    render json: { error: exam.errors.messages }, status: 422
+                end
 
-    def set_commentable
-        @commentable ||= Question.find(params[:question_id])
-        puts "@commentable #{@commentable}"
-    end
+                #redirect_to @commentable, notice: "Your comment was successfully posted."
+            end
+
+            private
+
+            def comment_params
+                params.require(:comment).permit(:text)
+            end
 end
+
+
+# class Api::V1::Questions::CommentsController < Api::V1::CommentsController
+#     before_action :set_commentable
+
+#     # def commentable
+#     #     @commentable ||= Question.find(params[:question_id])
+#     # end
+
+#     private
+
+#     def set_commentable
+#         @commentable ||= Question.find(params[:question_id])
+#         puts "@commentable #{@commentable}"
+#     end
+# end
 
 # ||= si no tiene valor no se va a ejecutar
