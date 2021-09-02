e17983919ed2d8c58e8887837a84964b4431601a
Luis <vsjoseluisvs@gmail.com>
2021-09-01 22:38:32 -0500

    comments deleted

--- app/controllers/api/v1/comments_question_controller.rb
+++ app/controllers/api/v1/comments_question_controller.rb
@@ -7,37 +7,3 @@ class Api::V1::CommentsQuestionController < ApplicationController
         @commentable ||= Question.find(params[:question_id])
     end
 end
-
-# module Api
-#     module V1
-#         class CommentsController < ApplicationController
-#             # protect_from_forgery with: :null_session
-#             # before_action :authenticate_user!
-            
-#             def index
-#                 comments = @commentable.comments.all
-            
-#                 render json: CommentSerializer.new(comments).serialized_json
-#             end
-
-#             def create
-#                 comment = @commentable.comments.new comment_params
-#                 # @comment.user = current_user
-
-#                 if comment.save
-#                     render json: CommentSerializer.new(comment).serialized_json
-#                 else
-#                     render json: { error: exam.errors.messages }, status: 422
-#                 end
-
-#                 #redirect_to @commentable, notice: "Your comment was successfully posted."
-#             end
-
-#             private
-
-#             def comment_params
-#                 params.require(:comment).permit(:text)
-#             end
-#         end
-#     end
-# end
--- app/controllers/api/v1/questions/comments_controller.rb
+++ app/controllers/api/v1/questions/comments_controller.rb
@@ -50,21 +50,3 @@ class Api::V1::Questions::CommentsController < Api::V1::CommentsQuestionControll
         params.require(:comment).permit(:text)
     end
 end
-
-
-# class Api::V1::Questions::CommentsController < Api::V1::CommentsController
-#     before_action :set_commentable
-
-#     # def commentable
-#     #     @commentable ||= Question.find(params[:question_id])
-#     # end
-
-#     private
-
-#     def set_commentable
-#         @commentable ||= Question.find(params[:question_id])
-#         puts "@commentable #{@commentable}"
-#     end
-# end
-
-# ||= si no tiene valor no se va a ejecutar
